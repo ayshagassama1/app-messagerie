e56c98450afb47da97ce64cb794d0baf9a320d01
SalySam <samake.saly@ugb.edu.sn>
2022-02-15 10:21:10 +0000

    inscription et authentification ok

---
+++ backend/config.php
@@ -0,0 +1,47 @@
+<?php
+function connexionBD() {
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=messagerie;charset=utf8', 'root', '');
+   // echo 'connexion reussie';
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+return $bdd;
+}
+
+function inscription($prenom, $nom, $email, $pass) {
+    $bdd = connexionBD();
+    $req = $bdd->prepare('INSERT INTO utilisateur (prenom,nom,email,pass) VALUES(?,?,?,?)');
+    $req->execute(array(htmlspecialchars($prenom),htmlspecialchars($nom),htmlspecialchars($email),htmlspecialchars($pass))); 
+    
+    if($req)
+    {
+        echo 'Inscription réussi';
+        return 1;
+    }
+    echo 'Erreur de traitement';
+    return 0;
+}
+
+function connexion($email, $pass){
+    $bdd = connexionBD();
+    $req = $bdd->prepare('SELECT * FROM utilisateur WHERE email=? AND pass=?');
+    $req->execute(array(htmlspecialchars($email),htmlspecialchars($pass)));
+    $reponse = $req->fetch();
+   // echo $reponse;
+    if($reponse){
+        $result  = $reponse['id'];
+       // echo 'bet'.$result;
+        return $result;
+    } else {
+       // echo 'connexion echoué';
+        return 0;
+    }
+}
+
+
+?>
+  
